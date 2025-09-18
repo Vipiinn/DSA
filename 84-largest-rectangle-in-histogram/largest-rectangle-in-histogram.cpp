d9f34dc44d9960247388822343fcf04cd2ee4eea
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-09-19 00:38:57 +0530

    Time: 36 ms (20.62%) | Memory: 85.8 MB (37.86%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+
+    void NextSmaller(vector<int>& heights , vector<int>&ans1){
+        int n = heights.size();
+
+        stack<int>st;
+
+        for(int i=n-1;i>=0;i--){
+
+            //pop
+            while(st.size() > 0 && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+
+            //store 
+            if (!st.empty()) {
+                ans1[i] = st.top();
+            } else {
+                ans1[i] = n; 
+            }
+
+            //push
+            st.push(i);
+        }
+
+    }
+
+    void previousSmaller(vector<int>& heights , vector<int>&ans2){
+        int n = heights.size();
+        stack<int>st;
+
+        for(int i=0;i<n;i++){
+
+            //pop
+            while(st.size() > 0 && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+
+            //store 
+            if (!st.empty()) {
+                ans2[i] = st.top();
+            } else {
+                ans2[i] = -1; 
+            }
+
+            //push
+            st.push(i);
+        }
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+
+        int n = heights.size();
+        if (n == 0) return 0;
+        if (n == 1) return heights[0];
+
+        vector<int>ans1(n);
+        vector<int>ans2(n);
+
+        NextSmaller(heights,ans1);
+        previousSmaller(heights,ans2);
+
+        int ans = INT_MIN;
+
+        for(int i=0;i<n;i++){
+            ans = max(ans,heights[i]*(ans1[i]-ans2[i]-1));
+        }
+        return ans;
+        
+    }
+};
