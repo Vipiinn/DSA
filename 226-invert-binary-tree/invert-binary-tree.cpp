c0e9b11e540ca4f0f6ebc89a84047f43bec8e1f8
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-09-28 14:28:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (59.61%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+
+    void helper(TreeNode* root){
+        if(root == NULL) return;
+        swap(root->left , root->right);
+        helper(root->left);
+        helper(root->right);
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        helper(root);
+        return root;
+    }
+};
