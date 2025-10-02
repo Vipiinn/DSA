38c349881996f6b6abed9f19e9dc93732f6f0ef1
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-10-02 19:46:02 +0530

    Time: 6 ms (5.85%) | Memory: 8.4 MB (77.56%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles;
+        int empty = numBottles;
+        int full = 0;
+        while(empty >= numExchange){
+            empty -= numExchange;
+            full++;
+            numExchange++;
+        }
+        ans += full;
+        empty += full;
+        full = 0;
+        // if(empty >= numExchange){
+        //     ans++;
+        // }
+        // return ans;
+        while(empty >= numExchange){
+            empty -= numExchange;
+            full++;
+            numExchange++;
+        }
+        ans += full;
+        return ans;
+    }
+};
