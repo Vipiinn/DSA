3e41cef92dbac67fad9c1d8bdb57e632522b0ab1
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-10-02 14:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (99.57%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+
+    int levels(TreeNode* root){
+        if(root == NULL) return 0;
+        return 1 + max(levels(root->left) , levels(root->right));
+    }
+
+    void preOrder(TreeNode* root , vector<int>& ans , int level){
+        if(root == NULL) return;
+        ans[level] = root->val;
+        preOrder(root->left , ans , level+1);
+        preOrder(root->right , ans , level+1);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        int n = levels(root);
+        vector<int>ans(n,0);
+        preOrder(root , ans , 0);
+        return ans;
+    }
+};
