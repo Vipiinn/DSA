3c5fc9ae1395a257e2ad12c67c814d641258ca27
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-10-04 15:29:37 +0530

    Time: 11 ms (13.48%) | Memory: 27.2 MB (84.41%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
 
-    TreeNode* helper(vector<int>& post, int postLow, int postHi , vector<int>& in , int inLow , int inHi){
+    TreeNode* build(vector<int>& post, int postLow, int postHi , vector<int>& in , int inLow , int inHi){
         if(postLow > postHi) return NULL;
 
         TreeNode* root = new TreeNode(post[postLow]);
@@ -17,8 +17,8 @@ public:
         int leftCount = i - inLow;
         int rightCount = inHi - i;
 
-        root->right = helper(post, postLow+1, postLow+rightCount , in , i+1 , inHi);
-        root->left = helper(post, postLow+rightCount+1, postHi , in , inLow , i-1);
+        root->right = build(post, postLow+1, postLow+rightCount , in , i+1 , inHi);
+        root->left = build(post, postLow+rightCount+1, postHi , in , inLow , i-1);
 
         return root;
     }
@@ -26,6 +26,6 @@ public:
     TreeNode* buildTree(vector<int>& in, vector<int>& post) {
         int n = post.size();
         reverse(post.begin() , post.end());
-        return helper(post , 0 , n-1 , in , 0 , n-1);
+        return build(post , 0 , n-1 , in , 0 , n-1);
     }
 };
