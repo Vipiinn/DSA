d88f9932426e97e9884584fbbb4fbf13ee53a1a8
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-10-10 18:28:43 +0530

    Time: 1 ms (5.99%) | Memory: 17.5 MB (77.72%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -1,34 +1,17 @@
 
 class Solution {
 public:
-
-    vector<TreeNode*> preorderTraversal(TreeNode* root) {
-
-        vector<TreeNode*>ans;
-
-        stack<TreeNode*>st;
-        st.push(root);
-        while(st.size() > 0){
-            TreeNode* temp = st.top();
-            if(temp!=NULL) ans.push_back(temp);
-            st.pop();
-
-            if(temp!=NULL && temp->right != NULL) st.push(temp->right);
-            if(temp!=NULL && temp->left != NULL) st.push(temp->left);
-        }
-
-        return ans;
-    }
-    
     void flatten(TreeNode* root) {
-
-        vector<TreeNode*>ans = preorderTraversal(root);
-        int n = ans.size();
-
-        for(int i=0;i<n-1;i++){
-            ans[i]->right = ans[i+1];
-            ans[i]->left = NULL;
-        }
-        
+        if(root == NULL) return;
+        TreeNode* l = root->left;
+        TreeNode* r = root->right;
+        root->left = NULL;
+        root->right = NULL;
+        flatten(l);
+        flatten(r);
+        root->right = l;
+        TreeNode* temp = root;
+        while(temp->right != NULL) temp = temp->right;
+        temp->right = r;
     }
 };
