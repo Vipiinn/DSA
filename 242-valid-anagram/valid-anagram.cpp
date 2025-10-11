4465c2d73a7fc9b52eae1e79ebf00d24b2e84992
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-10-11 18:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,4 +1,4 @@
-//using hashmap
+//using single hashmap
 
 class Solution {
 public:
@@ -6,27 +6,24 @@ public:
 
         if(s.length() != t.length()) return false;
 
-        unordered_map<char,int> map1;
-        unordered_map<char,int> map2;
+        unordered_map<char,int> map;
+        
 
         for(int i=0;i<s.length();i++){
-            map1[s[i]]++;
+            map[s[i]]++;
         }
 
         for(int i=0;i<t.length();i++){
-            map2[t[i]]++;
-        }
+            char ch = t[i];
 
-        for(auto x : map1){
-            char ch1 = x.first;
-            int freq1 = x.second;
-
-            if(map2.find(ch1) != map2.end()){
-                int freq2 = map2[ch1];
-                if(freq1 != freq2) return false;
+            if(map.find(ch) != map.end()){
+                map[ch]--;
+                if(map[ch] == 0) map.erase(ch);
             }
             else return false;
         }
+
+        if(map.size() > 0) return false;
         return true;
     }
 };
