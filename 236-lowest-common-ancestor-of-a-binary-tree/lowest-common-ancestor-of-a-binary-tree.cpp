d5cbbe6cca8a51a639bdac09667e6982471a8aaa
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-01-22 11:44:33 +0530

    Time: 14 ms (35.42%) | Memory: 17.2 MB (87.30%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,22 +1,15 @@
 
 class Solution {
 public:
-
-    bool isExists(TreeNode* root , TreeNode* target){
-        if(root == NULL) return false;
-        if(root == target) return true;
-        return isExists(root->left,target) || isExists(root->right,target);
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //base case
-        if(root == p || root == q) return root;
-        else if(isExists(root->left,p) && isExists(root->right , q)) return root;
-        else if(isExists(root->left,q) && isExists(root->right , p)) return root;
-        else if(isExists(root->left,p) &&  isExists(root->left , q)) return lowestCommonAncestor(root->left , p , q);
-        else{
-            return lowestCommonAncestor(root->right , p , q);
-        }
-        
+
+        if(root == NULL || root == p || q == root) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left , p , q);
+        TreeNode* right = lowestCommonAncestor(root->right , p , q);
+
+        if(left == NULL) return right;
+        else if(right == NULL) return left;
+        else return root;
     }
 };
