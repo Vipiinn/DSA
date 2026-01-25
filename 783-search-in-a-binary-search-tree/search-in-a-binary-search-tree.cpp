7199410391e6e95d189c11d4f00cc09f89023d6c
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-01-25 19:54:01 +0530

    Time: 0 ms (100.00%) | Memory: 35.4 MB (92.90%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -1,10 +1,13 @@
+
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
+        
         if(root == NULL) return NULL;
 
         if(root->val == val) return root;
-        else if(root->val < val) return searchBST(root->right , val);
-        else return searchBST(root->left , val);
+
+        if(root->val > val) return searchBST(root->left , val);
+        else  return searchBST(root->right , val);
     }
 };
