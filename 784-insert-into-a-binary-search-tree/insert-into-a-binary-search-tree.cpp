901800a93cb55a8d411f9ce9261a73d0cb8f04ff
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-01-26 21:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (50.13%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -2,25 +2,26 @@
 class Solution {
 public:
 
-    void helper(TreeNode* root, int val){
-
+    void helper(TreeNode* root , int val){
         if(root == NULL) return;
 
         if(root->val > val){
             if(root->left == NULL) root->left = new TreeNode(val);
             else helper(root->left , val);
         }
-        else{
+        else {
             if(root->right == NULL) root->right = new TreeNode(val);
             else helper(root->right , val);
         }
-
-       
-    }
+    }   
 
     TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
         if(root == NULL) return new TreeNode(val);
-        helper(root , val);
+
+        helper(root,val);
+
         return root;
+        
     }
 };
