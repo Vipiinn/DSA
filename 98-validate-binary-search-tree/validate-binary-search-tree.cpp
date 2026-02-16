08c11f49b355e7849bc0a2a1adb113cd2d40db59
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-02-16 13:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (16.79%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -2,18 +2,23 @@
 class Solution {
 public:
 
-    void preorder(TreeNode* root , vector<int>&ans){
-        if(root == NULL) return;
+    void Inorder(TreeNode* root , vector<int>& ans){
+        if(root == NULL) return ;
 
-        preorder(root->left , ans);
+        Inorder(root->left , ans);
         ans.push_back(root->val);
-        preorder(root->right , ans);
+        Inorder(root->right , ans);
+        
     }
 
     bool isValidBST(TreeNode* root) {
-        vector<int>ans;
-        preorder(root , ans);
-        for(int i=1;i<ans.size();i++){
+       
+        vector<int> ans;
+
+        Inorder(root,ans);
+
+        int n = ans.size();
+        for(int i=1;i<n;i++){
             if(ans[i-1] >= ans[i]) return false;
         }
 
