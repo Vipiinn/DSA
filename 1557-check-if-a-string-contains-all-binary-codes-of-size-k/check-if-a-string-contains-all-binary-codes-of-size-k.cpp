e19eb9f9312f3203bd61123716c842124e556667
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-02-23 23:54:13 +0530

    Time: 326 ms (73.37%) | Memory: 87.6 MB (72.46%) - LeetSync

---
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        int n = s.length();
+        
+        if (n - k + 1 < (1 << k))  
+            return false;
+        
+        unordered_set<string> st;
+        
+        for (int i = 0; i <= n - k; i++) {
+            st.insert(s.substr(i, k));
+        }
+        
+        return st.size() == (1 << k);
+    }
+};
