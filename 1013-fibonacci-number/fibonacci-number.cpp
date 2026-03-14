135c6fa862817c4412dfe931e358100a3c3ce37a
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-03-14 20:32:51 +0530

    Time: 2 ms (78.74%) | Memory: 7.6 MB (87.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,25 +1,20 @@
-//tabultation method
+//Extream optimal
 
 class Solution {
 public:
-
-    int fibo(int n , vector<int>& dp){
-
-        dp[0] = 0;
-        dp[1]=  1;
-
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
-    }
-
     int fib(int n) {
 
         if(n <= 1) return n;
-        
-        vector<int>dp(n+1);
 
-        return fibo(n , dp);
+        int prev2 = 0;
+        int prev1 = 1;
+
+        for(int i=2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 };
