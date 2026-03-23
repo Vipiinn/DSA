62bfa44fb21eda18f46d90a56195e088d65d260f
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-03-23 13:45:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (64.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
 
-    int fibo(int n , vector<int>& dp){
-        if(n <= 1) return n;
-
-        if(dp[n] != -1){
-            return dp[n];
-        }
-
-        dp[n] =  fibo(n-1 , dp) + fibo(n-2 , dp);
-        return dp[n];
-    }
+    
 
     int fib(int n) {
 
-        vector<int>dp(n+1 , -1);
+        if(n <= 1) return n; 
         
-        return fibo(n , dp);
+        int prev1 = 1;
+        int prev2 = 0;
+
+        
+        for(int i=2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 };
