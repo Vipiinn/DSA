a5df7513caddd105073a6cd3636ecdfa71119c13
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-03-23 14:10:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (67.07%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int solve(vector<int>& nums , int idx , vector<int>&dp){
+
+        if(idx >= nums.size()) return 0;
+
+        if(dp[idx] != -1) return dp[idx];
+
+        dp[idx] = max(nums[idx] + solve(nums,idx+2,dp) , solve(nums , idx+1 , dp));
+        return dp[idx];
+    }
+
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int>dp(n+1 , -1);
+        
+        return solve(nums , 0 , dp);
+    }
+};
