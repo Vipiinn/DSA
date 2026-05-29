2f375743d7fad362e177a8915885a6669f62ad7b
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-05-29 19:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.06%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int st = 0;
+        int end = row * col - 1;
+
+        while(st <= end){
+
+            int mid = st + (end - st) / 2;
+            int ele = matrix[mid/col][mid%col];
+
+            if(ele == target){
+                return true;
+            }
+            else if(ele > target){
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+
+        }
+
+        return false;
+    }
+};
